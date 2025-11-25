5cd49cdcd145629abc00872b9f6abac00c6df5ab
SATYAVOLU MYTHRI <mythri.allur@gmail.com>
2025-11-25 16:13:06 +0530

    Add files via upload

---
+++ task1-background change when clicked on button/script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
